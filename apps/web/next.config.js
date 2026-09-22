d34f6b78a77481663396127cdc54472f3d3b6cfc
Tajudeen <tajud@users.noreply.github.com>
2026-09-22 10:44:58 +0100

    ci: deploy web app to GitHub Pages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,19 +1,32 @@
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
   outputFileTracingRoot: path.join(__dirname, '../../'),
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+  ...(isGitHubPages
+    ? {
+      output: 'export',
+      basePath: '/precursor',
+      trailingSlash: true,
+    }
+    : {
+      async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:3001/api/:path*',
+          },
+          {
+            source: '/ws',
+            destination: 'http://localhost:3001/ws',
+          },
+        ];
       },
-      {
-        source: '/ws',
-        destination: 'http://localhost:3001/ws',
-      },
-    ];
+    }),
+  images: {
+    unoptimized: true,
   },
 };
 
